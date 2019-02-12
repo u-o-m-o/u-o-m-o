84eaeb2945a5c7dc507caa5da5ef2ee27c7f6a3d
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-02-12 14:02:20 +0100

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -66,5 +66,82 @@ $(document).ready(function() {
 
         }
     })
+        
+    $.ajax({
+            dataType: "json",
+            url: "attributisocial.json",
+            success: function(datisegn) {
+                        var socialname = [];
+                        var attributiunici = [];
+                        var attributi = [];
+                        var percentualecompletezza = [];
+                        datisegn.forEach(function(arrayItem) {
+                            socialname.push(arrayItem["social"])
+                            attributiunici.push(arrayItem["attributi unici"])
+                            attributi.push(arrayItem["attributi"])
+                            percentualecompletezza.push(Math.round(arrayItem["percentualecompletezza"]))
+                        });
+                        var datibaratt = {
+                        labels: featureslabels,
+                        datasets: [
+                            {
+                                "label": "Features",
+                                fontColor: "black",
+                                data: attributi,
+                                backgroundColor: "#808080"
+                                },
+                                {
+                                    "label": "Unique Features",
+                                    fontColor: "black",
+                                    data: attributiunici,
+                                    backgroundColor: "#808080"
+                                    },
+                                ]
+                        };
 
+                        var opzionibaratt = {
+                                            responsiveAnimationDuration: 1000,
+                                            "tooltips": {
+                                                "enabled": true,
+                                                "mode": 'single',
+                                                "callbacks": {
+                                                    "label": function(tooltipItems, data) {
+                                                        return data.datasets[tooltipItems.datasetIndex].label + ': ' + tooltipItems.xLabel + '%';
+                                                    }
+                                                }
+                                            },
+                                            legend: {
+                                                fontColor: "black"
+                                            },
+                                            "scales": {
+                                                "xAxes": [{
+                                                    "ticks": {
+                                                        fontColor: "black",
+                                                        "beginAtZero": true,
+                                                        "max": 35,
+                                                        "callback": function(value, index, values) {
+                                                            return value + '%';
+                                                        }
+                                                    }
+                                                }],
+                                            "yAxes": [{
+                                                    "ticks": {
+                                                        fontColor: "black"}
+                                                }]
+                                            }
+                        };
+
+
+
+                    var barratt = document.getElementById("FeaturesChartbar").getContext("2d");
+
+                    new Chart(barratt,{
+                                                "type": "horizontalBar",
+                                                "data": datibaratt,
+                                                "options": opzionibaratt
+                                            });
+
+        }
+    })
+    
 })
